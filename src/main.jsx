2b8b49528c9b36eb0f0b42499b889879fb817edc
shahjoyal <132144905+shahjoyal@users.noreply.github.com>
2025-03-25 20:22:22 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const meta = document.createElement('meta');
+meta.name = "viewport";
+meta.content = "width=device-width, initial-scale=1";
+document.head.appendChild(meta); // Dynamically adding viewport meta
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
